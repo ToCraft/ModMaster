baa01a90c0dc9f6d19fe419ded8126989cde0071
To_Craft <development@tocraft.dev>
2025-10-10 14:56:31 +0200

    add dummy src

--- src/main/kotlin/dev/tocraft/modmaster/general.gradle.kts
+++ src/main/kotlin/dev/tocraft/modmaster/general.gradle.kts
@@ -13,6 +13,24 @@ extensions.configure<LoomGradleExtensionAPI> {
     silentMojangMappingsLicense()
 }
 
+// dummy source set so I do not need to implement any mod dependency at all - I just need dummies
+val dummySource: SourceSet by sourceSets.creating {
+    java.srcDir("src/dummy/java")
+
+    // Only inherit main's dependencies (not output) to avoid circularity
+    compileClasspath += sourceSets["main"].compileClasspath
+    runtimeClasspath += sourceSets["main"].runtimeClasspath
+}
+
+// compile dummy before main
+tasks.named("compileJava") {
+    dependsOn(tasks.named("compileDummySourceJava"))
+}
+
+// make main use dummy
+sourceSets["main"].compileClasspath += dummySource.output
+sourceSets["main"].runtimeClasspath += dummySource.output
+
 dependencies {
     minecraft("com.mojang:minecraft:${parent!!.properties["minecraft"]}")
     mappings(loom.layered {
